93d0960c35bc402137cc319fd73d84fcf797324d
charliekump-wf <charlie.kump@webfilings.com>
2017-09-19 11:25:20 -0600

    Add option to write repos to file.

--- darthub.go
+++ darthub.go
@@ -1,21 +1,27 @@
 package main
 
 import (
-	"github.com/google/go-github/github"
 	"context"
-	"golang.org/x/oauth2"
-	"os"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"os"
+	"strings"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
 )
 
+var cutRepoURLs []string = make([]string, 0)
+
 func usage() {
-	println("darthub <user>")
+	println("darthub <user> <write_file>")
 }
 
 func main() {
 	flag.Parse()
 	userName := flag.Arg(0)
+	fileFlag := flag.Arg(1)
 
 	if userName == "" {
 		usage()
@@ -40,14 +46,31 @@ func main() {
 	searcher := GithubSearcher{client: client}
 
 	results, err := searcher.Search(SearchParams{
-		User: userName,
-		Filename: "pubspec",
+		User:      userName,
+		Filename:  "pubspec",
 		Extension: "yaml",
-		PerPage: RESULTS_PER_PAGE,
+		PerPage:   RESULTS_PER_PAGE,
 	})
 	if err != nil {
 		fmt.Printf("Error: %s\n", err)
 		os.Exit(1)
+	}
+
+	if fileFlag != "" {
+		for _, result := range results {
+		cutRepoURL := strings.Split(result.URL, "github.com/")[1]
+		println(cutRepoURL)
+		cutRepoURLs = append(cutRepoURLs, cutRepoURL)
+	}
+
+		file, err := os.Create("dart_medic_repo_list.py")
+		if err == nil {
+			data := json.NewEncoder(file)
+			data.SetIndent("", "  ")
+			err = data.Encode(cutRepoURLs)
+		} else {
+			fmt.Printf("Error: %s\n", err)
+		}
 	}
 
 	for _, result := range results {
